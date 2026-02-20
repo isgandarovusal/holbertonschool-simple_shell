174788817b9a7b961af6afa7f1cb2cb706049cd0
Vusal Isgandarov <vusalisgandarov@gmail.com>
2026-02-20 15:07:15 +0000

    changing shell.c execurot.c

--- executor.c
+++ executor.c
@@ -1,33 +1,48 @@
-void execute(char **args) {
+#include "shell.h"
+
+/**
+ * execute - forks and executes a command
+ * @args: array of arguments
+ */
+void execute(char **args)
+{
     char *actual_command;
     pid_t child_pid;
     int status;
 
-    /* 1. Find the path FIRST */
+    if (args == NULL || args[0] == NULL)
+        return;
+
+    /* Find path before forking */
     actual_command = _which(args[0]);
 
-    if (actual_command == NULL) {
-        /* Command not found - PRINT ERROR AND RETURN */
-        /* DO NOT FORK HERE */
-        perror("Error"); 
+    if (actual_command == NULL)
+    {
+        perror("./hsh"); /* Or your specific error format */
         return;
     }
 
-    /* 2. ONLY fork if the command was actually found */
     child_pid = fork();
-    if (child_pid == -1) {
-        perror("Fork failed");
+    if (child_pid == -1)
+    {
+        perror("Error:");
+        if (actual_command != args[0])
+            free(actual_command);
         return;
     }
 
-    if (child_pid == 0) {
-        if (execve(actual_command, args, environ) == -1) {
-            perror("Execve failed");
+    if (child_pid == 0)
+    {
+        if (execve(actual_command, args, environ) == -1)
+        {
+            perror("Error:");
             exit(EXIT_FAILURE);
         }
-    } else {
+    }
+    else
+    {
         wait(&status);
-        /* Don't forget to free actual_command if it was malloc'd in path_finder */
+        /* Free if _which returned a newly allocated string */
         if (actual_command != args[0])
             free(actual_command);
     }
--- shell.h
+++ shell.h
@@ -8,21 +8,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <errno.h>
+
+/* Access to the environment strings */
+extern char **environ;
 
 /* Function prototypes */
-void display_prompt(void);
-char *read_line(void);
-char **tokenize_line(char *line);
-void free_args(char **args);
-char *find_command_path(char *cmd, char **env);
-char *get_path_env(char **env);
-char *build_path(char *dir, char *cmd);
-int check_file_exists(char *path);
-char *find_in_path(char *cmd, char **env);
-void execute_command(char **args, char **env, char *program_name);
-void print_not_found(char *program_name, char *command);
-int is_exit_command(char **args);
-void handle_exit(char **args);
+char *_which(char *command);
+void execute(char **args);
 
-#endif /* SHELL_H */
+#endif
